70d750e1f5cfe2ba814112867769dcc9378d02a0
sunjianxiang <1504734652@qq.com>
2025-04-15 16:37:17 +0800

    添加 UEditorPlus 插件和主题所需的图像及样式文件，更新 package.json 以引入 vue-ueditor-wrap

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import '@/assets/css/index.css'
 import App from './App.vue'
 import router from "./router";
 import { loadingDirective } from "@/directives/loading/install"
+import VueUeditorWrap from 'vue-ueditor-wrap';
 
 
 const app = createApp(App);
@@ -11,6 +12,7 @@ const app = createApp(App);
 
 
 app.use(router);
+app.use(VueUeditorWrap);
 // 注册全局loading指令
 app.directive("loading", loadingDirective)
 app.mount('#app');
--- src/views/console/article/editArticle/index.vue
+++ src/views/console/article/editArticle/index.vue
@@ -6,459 +6,73 @@
 -->
 <template>
   <div class="flex-1 overflow-hidden">
-    <div class="main-container prose max-w-full !h-full">
-      <div
-        class="editor-container editor-container_document-editor editor-container_include-style editor-container_include-word-count"
-        ref="editorContainerElement">
-        <div class="relative">
-          <div class="editor-container__menu-bar" ref="editorMenuBarElement"></div>
-          <div class="absolute top-0 bottom-0 right-4 text-[13px] btn-wrapper" v-if="cloud.data">
-            <SubmitForm :content="content"></SubmitForm>
-          </div>
-        </div>
-        <div class="editor-container__toolbar" ref="editorToolbarElement"></div>
-        <div class="editor-container__editor-wrapper">
-          <div class="editor-container__editor">
-            <div ref="editorElement">
-              <ckeditor v-if="editor && config" :editor="editor" :config="config" @ready="onReady" v-model="content" />
-            </div>
-          </div>
-        </div>
-        <div class="editor_container__word-count" ref="editorWordCountElement"></div>
-      </div>
-    </div>
+    <vue-ueditor-wrap v-model="content" editor-id="editor" :config="editorConfig"
+      :editorDependencies="['ueditor.config.js', 'ueditor.all.js']" style="height:500px;" />
   </div>
 </template>
 
-<script setup>
-import "./assets/style.css"
-import { computed, ref, onMounted, useTemplateRef } from 'vue';
-import { Ckeditor, useCKEditorCloud } from '@ckeditor/ckeditor5-vue';
+<script setup lang="ts">
+import { useRoute } from "vue-router";
 import SubmitForm from "./SubmitForm.vue";
+import { computed, onMounted, ref } from "vue";
 import { articleDetailData } from "@/service/article";
-import { useRoute, useRouter } from "vue-router";
-const LICENSE_KEY = ''
-  // 'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3NzA5NDA3OTksImp0aSI6IjJmYTM3ODg5LWUyNDAtNGViYi1hNzNlLTg4YWFlMDFkMmZiYSIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIl0sInZjIjoiMTJkOGJjODYifQ.XRlQdBnH0huT1L0EpkJdscsev3LI0XV19T26a9dTOuJZsBwGNnEs4ajVjGNLHx5q0RUl5XLwnfltJxwd43f-JQ';
-
-const editorToolbar = useTemplateRef('editorToolbarElement');
-const editorMenuBar = useTemplateRef('editorMenuBarElement');
-const editorWordCount = useTemplateRef('editorWordCountElement');
-
-const cloud = useCKEditorCloud({ version: '44.3.0' });
-
-const isLayoutReady = ref(false);
-const isEditorReady = ref(false);
-
-const editor = computed(() => {
-  if (!cloud.data.value) {
-    return null;
-  }
-
-  return cloud.data.value.CKEditor.DecoupledEditor;
-});
-
-const config = computed(() => {
-  if (!isLayoutReady.value) {
-    return null;
-  }
-
-  if (!cloud.data.value) {
-    return null;
-  }
-
-  const {
-    Alignment,
-    Autoformat,
-    AutoImage,
-    AutoLink,
-    Autosave,
-    BalloonToolbar,
-    BlockQuote,
-    Bold,
-    Bookmark,
-    Code,
-    CodeBlock,
-    Emoji,
-    Essentials,
-    FindAndReplace,
-    FontBackgroundColor,
-    FontColor,
-    FontFamily,
-    FontSize,
-    GeneralHtmlSupport,
-    Heading,
-    Highlight,
-    HorizontalLine,
-    HtmlComment,
-    HtmlEmbed,
-    ImageBlock,
-    ImageCaption,
-    ImageEditing,
-    ImageInline,
-    ImageInsert,
-    ImageInsertViaUrl,
-    ImageResize,
-    ImageStyle,
-    ImageTextAlternative,
-    ImageToolbar,
-    ImageUpload,
-    ImageUtils,
-    Indent,
-    IndentBlock,
-    Italic,
-    Link,
-    LinkImage,
-    List,
-    ListProperties,
-    MediaEmbed,
-    Mention,
-    PageBreak,
-    Paragraph,
-    PasteFromMarkdownExperimental,
-    PasteFromOffice,
-    RemoveFormat,
-    ShowBlocks,
-    SimpleUploadAdapter,
-    SpecialCharacters,
-    SpecialCharactersArrows,
-    SpecialCharactersCurrency,
-    SpecialCharactersEssentials,
-    SpecialCharactersLatin,
-    SpecialCharactersMathematical,
-    SpecialCharactersText,
-    Strikethrough,
-    Style,
-    Subscript,
-    Superscript,
-    Table,
-    TableCaption,
-    TableCellProperties,
-    TableColumnResize,
-    TableProperties,
-    TableToolbar,
-    TextPartLanguage,
-    TextTransformation,
-    TodoList,
-    Underline,
-    WordCount
-  } = cloud.data.value.CKEditor;
-
-  return {
-    toolbar: {
-      items: [
-        'showBlocks',
-        '|',
-        'heading',
-        'style',
-        '|',
-        'fontSize',
-        'fontFamily',
-        'fontColor',
-        'fontBackgroundColor',
-        '|',
-        'bold',
-        'italic',
-        'underline',
-        '|',
-        'link',
-        'insertImage',
-        'insertTable',
-        'highlight',
-        'blockQuote',
-        'codeBlock',
-        '|',
-        'alignment',
-        '|',
-        'bulletedList',
-        'numberedList',
-        'todoList',
-        'outdent',
-        'indent'
-      ],
-      shouldNotGroupWhenFull: false
-    },
-    plugins: [
-      Alignment,
-      Autoformat,
-      AutoImage,
-      AutoLink,
-      Autosave,
-      BalloonToolbar,
-      BlockQuote,
-      Bold,
-      Bookmark,
-      Code,
-      CodeBlock,
-      Emoji,
-      Essentials,
-      FindAndReplace,
-      FontBackgroundColor,
-      FontColor,
-      FontFamily,
-      FontSize,
-      GeneralHtmlSupport,
-      Heading,
-      Highlight,
-      HorizontalLine,
-      HtmlComment,
-      HtmlEmbed,
-      ImageBlock,
-      ImageCaption,
-      ImageEditing,
-      ImageInline,
-      ImageInsert,
-      ImageInsertViaUrl,
-      ImageResize,
-      ImageStyle,
-      ImageTextAlternative,
-      ImageToolbar,
-      ImageUpload,
-      ImageUtils,
-      Indent,
-      IndentBlock,
-      Italic,
-      Link,
-      LinkImage,
-      List,
-      ListProperties,
-      MediaEmbed,
-      Mention,
-      PageBreak,
-      Paragraph,
-      PasteFromMarkdownExperimental,
-      PasteFromOffice,
-      RemoveFormat,
-      ShowBlocks,
-      SimpleUploadAdapter,
-      SpecialCharacters,
-      SpecialCharactersArrows,
-      SpecialCharactersCurrency,
-      SpecialCharactersEssentials,
-      SpecialCharactersLatin,
-      SpecialCharactersMathematical,
-      SpecialCharactersText,
-      Strikethrough,
-      Style,
-      Subscript,
-      Superscript,
-      Table,
-      TableCaption,
-      TableCellProperties,
-      TableColumnResize,
-      TableProperties,
-      TableToolbar,
-      TextPartLanguage,
-      TextTransformation,
-      TodoList,
-      Underline,
-      WordCount
-    ],
-    balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage', '|', 'bulletedList', 'numberedList'],
-    fontFamily: {
-      supportAllValues: true
-    },
-    fontSize: {
-      options: [10, 12, 14, 'default', 18, 20, 22],
-      supportAllValues: true
-    },
-    heading: {
-      options: [
-        {
-          model: 'paragraph',
-          title: 'Paragraph',
-          class: 'ck-heading_paragraph'
-        },
-        {
-          model: 'heading1',
-          view: 'h1',
-          title: 'Heading 1',
-          class: 'ck-heading_heading1'
-        },
-        {
-          model: 'heading2',
-          view: 'h2',
-          title: 'Heading 2',
-          class: 'ck-heading_heading2'
-        },
-        {
-          model: 'heading3',
-          view: 'h3',
-          title: 'Heading 3',
-          class: 'ck-heading_heading3'
-        },
-        {
-          model: 'heading4',
-          view: 'h4',
-          title: 'Heading 4',
-          class: 'ck-heading_heading4'
-        },
-        {
-          model: 'heading5',
-          view: 'h5',
-          title: 'Heading 5',
-          class: 'ck-heading_heading5'
-        },
-        {
-          model: 'heading6',
-          view: 'h6',
-          title: 'Heading 6',
-          class: 'ck-heading_heading6'
-        }
-      ]
-    },
-    htmlSupport: {
-      allow: [
-        {
-          name: /^.*$/,
-          styles: true,
-          attributes: true,
-          classes: true
-        }
-      ]
-    },
-    image: {
-      toolbar: [
-        'toggleImageCaption',
-        'imageTextAlternative',
-        '|',
-        'imageStyle:inline',
-        'imageStyle:wrapText',
-        'imageStyle:breakText',
-        '|',
-        'resizeImage'
-      ]
-    },
-    initialData: '',
-    licenseKey: LICENSE_KEY,
-    link: {
-      addTargetToExternalLinks: true,
-      defaultProtocol: 'https://',
-      decorators: {
-        toggleDownloadable: {
-          mode: 'manual',
-          label: 'Downloadable',
-          attributes: {
-            download: 'file'
-          }
-        }
-      }
-    },
-    list: {
-      properties: {
-        styles: true,
-        startIndex: true,
-        reversed: true
-      }
-    },
-    mention: {
-      feeds: [
-        {
-          marker: '@',
-          feed: [
-            /* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
-          ]
-        }
-      ]
-    },
-    menuBar: {
-      isVisible: true
-    },
-    placeholder: 'Type or paste your content here!',
-    style: {
-      definitions: [
-        {
-          name: 'Article category',
-          element: 'h3',
-          classes: ['category']
-        },
-        {
-          name: 'Title',
-          element: 'h2',
-          classes: ['document-title']
-        },
-        {
-          name: 'Subtitle',
-          element: 'h3',
-          classes: ['document-subtitle']
-        },
-        {
-          name: 'Info box',
-          element: 'p',
-          classes: ['info-box']
-        },
-        {
-          name: 'Side quote',
-          element: 'blockquote',
-          classes: ['side-quote']
-        },
-        {
-          name: 'Marker',
-          element: 'span',
-          classes: ['marker']
-        },
-        {
-          name: 'Spoiler',
-          element: 'span',
-          classes: ['spoiler']
-        },
-        {
-          name: 'Code (dark)',
-          element: 'pre',
-          classes: ['fancy-code', 'fancy-code-dark']
-        },
-        {
-          name: 'Code (bright)',
-          element: 'pre',
-          classes: ['fancy-code', 'fancy-code-bright']
-        }
-      ]
-    },
-    table: {
-      contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
-    }
-  };
-});
 const route = useRoute()
 const id = computed(() => route.query.id)
 onMounted(() => {
-  isLayoutReady.value = true;
   getArticle()
 });
-const getArticle = async()=>{
+const getArticle = async () => {
   if (!id.value) return;
   const article = await articleDetailData(id.value)
   content.value = article.content
-  
+
 }
-function onReady(editor) {
-  [...editorWordCount.value.children].forEach(child => child.remove());
-  [...editorToolbar.value.children].forEach(child => child.remove());
-  [...editorMenuBar.value.children].forEach(child => child.remove());
+const content = ref("")
+let editorConfig = {
+  loadConfigFromServer: false,
+  // 图片限制最大3M
+  imageMaxSize: 3145728,
+  // 富文本输入框高度
+  initialFrameHeight: 500,
+  // 富文本输入框宽度
+  initialFrameWidth: '100%',
 
-  const wordCount = editor.plugins.get('WordCount');
-  editorWordCount.value.appendChild(wordCount.wordCountContainer);
-  editorToolbar.value.appendChild(editor.ui.view.toolbar.element);
-  editorMenuBar.value.appendChild(editor.ui.view.menuBarView.element);
-  isEditorReady.value = true
-}
-
-const content = ref('');
-</script>
-
-<style scoped lang="scss">
-.container {
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  font-family: "oppo-sans";
-  overflow: hidden;
-
-  .middle {
-    flex: 1;
+  // 获取上传配置路径
+  configUrl: import.meta.env.VITE_API_URL + '/info/uEditor/config',
+  // 上传服务路径
+  serverUrl: import.meta.env.VITE_API_URL + '/info/uEditor/upload',
+  // 必须配置域名,否则发版后获取不到页面资源
+  UEDITOR_HOME_URL: location.origin + '/UEditorPlus/',
+  UEDITOR_CORS_URL: location.origin + '/UEditorPlus/',
+  // 配置请求头token
+  serverHeaders: {
+    'Authorization': `bearer `
+  },
+  // 上传图片配置
+  imageConfig: {
+    // 禁止在线管理
+    disableOnline: true,
   }
 }
 
-.btn-wrapper {
-  padding: var(--ck-spacing-small);
-}
-</style>
+// 弹窗逻辑
+const showSaveDialog = () => {
+  alert("保存功能尚未实现！");
+};
+
+// 监听 Cmd + S 快捷键
+const saveShortcutHandler = (event: KeyboardEvent) => {
+  if ((event.metaKey || event.ctrlKey) && event.key === "s") {
+    event.preventDefault(); // 阻止默认保存行为
+    showSaveDialog();
+  }
+};
+
+const setupSaveShortcut = () => {
+  window.addEventListener("keydown", saveShortcutHandler);
+};
+
+const removeSaveShortcut = () => {
+  window.removeEventListener("keydown", saveShortcutHandler);
+};
+</script>
