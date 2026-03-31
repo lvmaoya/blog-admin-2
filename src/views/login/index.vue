fc99397e7e49dfb8fa79ddf27738f246da8cb2f5
sunjianxiang <1504734652@qq.com>
2026-03-31 13:35:20 +0800

    style(login): 调整登录表单的上下边距
    将登录表单的上下边距从50px调整为60px，以改善页面的视觉平衡和间距一致性。

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -243,7 +243,7 @@ onMounted(() => {
         }
 
         form {
-          margin: 50px 0;
+          margin: 60px 0;
           width: 100%;
           display: flex;
           flex-direction: column;
